b80b04bea93decbe5e8f7e12c65994d944195bd1
Erik Kapitány <kapitanyerik@gmail.com>
2021-03-31 19:32:25 +0200

    Initial Commit

---
+++ startUp/startUpApp.swift
@@ -0,0 +1,17 @@
+//
+//  startUpApp.swift
+//  startUp
+//
+//  Created by Erik Kapitány on 2021. 03. 31..
+//
+
+import SwiftUI
+
+@main
+struct startUpApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
